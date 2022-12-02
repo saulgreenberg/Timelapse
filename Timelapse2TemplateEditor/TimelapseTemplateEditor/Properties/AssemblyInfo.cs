002f882a362de15071202152f462d8dfa3f25c39
saulgreenberg <saul@ucalgary.ca>
2022-12-02 09:58:48 -0700

    Changed version number to 2.3.0.3

--- Timelapse2TemplateEditor/TimelapseTemplateEditor/Properties/AssemblyInfo.cs
+++ Timelapse2TemplateEditor/TimelapseTemplateEditor/Properties/AssemblyInfo.cs
@@ -30,8 +30,8 @@ using System.Windows;
                                               // app, or any theme specific resource dictionaries)
 )]
 
-[assembly: AssemblyVersion("2.3.0.2")]
-[assembly: AssemblyFileVersion("2.3.0.2")]
+[assembly: AssemblyVersion("2.3.0.3")]
+[assembly: AssemblyFileVersion("2.3.0.3")]
 
 [assembly: InternalsVisibleTo("Timelapse2.UnitTests")]
 [assembly: NeutralResourcesLanguage("en-US")]
